e2817c54ab3aae248cb74f94bd7552c9abd27997
Evandro Myller <evandro.myller@flagsmith.com>
2025-10-01 13:40:09 -0300

    yay

--- src/Engine/Utils/Types/Context/FeatureContext.php
+++ src/Engine/Utils/Types/Context/FeatureContext.php
@@ -2,10 +2,8 @@
 
 namespace Flagsmith\Engine\Utils\Types\Context;
 
-use JsonSerializable;
-
 // TODO: Port this to https://wiki.php.net/rfc/dataclass
-class FeatureContext implements JsonSerializable
+class FeatureContext
 {
     /** @var string */
     public $key;
@@ -27,26 +25,4 @@ class FeatureContext implements JsonSerializable
 
     /** @var array<FeatureValue> */
     public $variants;
-
-    /** @return array<string, mixed> */
-    public function jsonSerialize(): array
-    {
-        $json = [
-            'key' => $this->key,
-            'feature_key' => $this->feature_key,
-            'name' => $this->name,
-            'enabled' => $this->enabled,
-            'value' => $this->value,
-        ];
-
-        if ($this->priority !== null) {
-            $json['priority'] = $this->priority;
-        }
-
-        if ($this->variants) {
-            $json['variants'] = $this->variants;
-        }
-
-        return $json;
-    }
 }
